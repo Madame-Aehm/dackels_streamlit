20c6009159dae844e42a841b5cc256ac8f3576b4
Madame-Aehm <emilyrachelstickler@gmail.com>
2025-02-10 14:26:47 +0100

    exclaimations

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Hello World")
+st.title("Hello World!!!!")
 st.text("this is some more text", help="this is a tooltip")
 
 selected_date = st.date_input("Choose a date", "today")
